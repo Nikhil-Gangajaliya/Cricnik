7080dee6a3c6915932dd69d0271f8b58580ffb3d
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2026-04-28 17:00:06 +0530

    final pwa setup

--- cricnik-frontend/src/main.jsx
+++ cricnik-frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log("New version available");
+  },
+  onOfflineReady() {
+    console.log("App ready to work offline");
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
