a6b5381d162f254acabe2754b0e51d6789dfefaf
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2026-04-30 18:06:16 +0530

    feat: update background image and enhance styling for login and signup pages

--- cricnik-frontend/src/pages/Login.jsx
+++ cricnik-frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../api/api";
 import "./pages.css";
-import bgImage from "../assets/bg.png";
+import bgImage from "../assets/bg2.png";
 
 export default function Login() {
   const [email, setEmail] = useState("");
